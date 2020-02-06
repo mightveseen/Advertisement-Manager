de7ca53d391b715202dd8eb5bf0901c0b0432a0f
petushok_valiantsin <blackvalik@gmail.com>
2020-02-06 20:43:06 +0300

    HotelUI

---
+++ HotelUI/src/com/senlainc/git_courses/java_training/petushok_valiantsin/ui/MenuItem.java
@@ -0,0 +1,47 @@
+package com.senlainc.git_courses.java_training.petushok_valiantsin.ui;
+
+public class MenuItem {
+    private final String title;
+    private Menu nextMenu;
+    private int id;
+    private IAction action;
+
+    public MenuItem(String title, IAction action) {
+        this.title = title;
+        this.action = action;
+    }
+
+    public MenuItem(String title, Menu nextMenu) {
+        this.title = title;
+        this.nextMenu = nextMenu;
+    }
+
+    public MenuItem(String title, IAction action, Menu nextMenu) {
+        this.title = title;
+        this.action = action;
+        this.nextMenu = nextMenu;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int index) {
+        this.id = index;
+    }
+
+    public void doAction() {
+        if (action != null) {
+            action.execute();
+        }
+    }
+
+    public Menu getNextMenu() {
+        return this.nextMenu;
+    }
+
+    @Override
+    public String toString() {
+        return id + ")" + title;
+    }
+}
