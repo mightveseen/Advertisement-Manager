d2dba8b2be2446b827a115b75790576de525a609
petushok_valiantsin <blackvalik@gmail.com>
2020-02-06 20:19:00 +0300

    Dependency Injection

---
+++ DependencyInjection/src/com/senlainc/git_courses/java_training/petushok_valiantsin/annotation/ConfigProperty.java
@@ -0,0 +1,14 @@
+package com.senlainc.git_courses.java_training.petushok_valiantsin.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.FIELD)
+@Retention(value = RetentionPolicy.RUNTIME)
+public  @interface ConfigProperty {
+    String configName();
+    String propertyName();
+    Class type();
+}
