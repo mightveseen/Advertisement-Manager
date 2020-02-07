eda662df41bdebd3172c3cc00818dda9a35fba8b
petushok_valiantsin <blackvalik@gmail.com>
2020-02-08 00:35:16 +0300

    First task - improvements

---
+++ DependencyInjection/src/com/senlainc/git_courses/java_training/petushok_valiantsin/Main.java
@@ -0,0 +1,9 @@
+package com.senlainc.git_courses.java_training.petushok_valiantsin;
+
+import com.senlainc.git_courses.java_training.petushok_valiantsin.configuration.Try;
+
+public class Main {
+    public static void main(String[] args) {
+        new Try().configTry();
+    }
+}
--- DependencyInjection/src/com/senlainc/git_courses/java_training/petushok_valiantsin/configuration/Main.java
+++
@@ -1,7 +0,0 @@
-package com.senlainc.git_courses.java_training.petushok_valiantsin.configuration;
-
-public class Main {
-    public static void main(String[] args) {
-        new Try().configTry();
-    }
-}
