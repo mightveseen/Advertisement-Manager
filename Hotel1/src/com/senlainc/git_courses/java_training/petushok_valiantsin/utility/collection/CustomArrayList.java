b64f1641bbaa845301a7bb4549b7f13cfc846c4c
petushok_valiantsin <blackvalik@gmail.com>
2020-02-19 23:11:46 +0300

    Improvement's

--- Hotel1/src/com/senlainc/git_courses/java_training/petushok_valiantsin/utility/collection/CustomArrayList.java
+++ Hotel1/src/com/senlainc/git_courses/java_training/petushok_valiantsin/utility/collection/CustomArrayList.java
@@ -1,9 +1,13 @@
 package com.senlainc.git_courses.java_training.petushok_valiantsin.utility.collection;
 
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
-public class CustomArrayList<T> implements ICustomList<T> {
+public class CustomArrayList<T> implements List<T> {
     private final int INIT_SIZE = 10;
     private int index = 0;
     private T[] objArray = (T[]) new Object[INIT_SIZE];
@@ -12,33 +16,58 @@ public class CustomArrayList<T> implements ICustomList<T> {
 
     }
 
-    public CustomArrayList(ICustomList<T> list) {
+    public CustomArrayList(List<T> list) {
         addAll(list);
     }
 
     @Override
-    public void add(T element) {
+    public boolean add(T element) {
         checkSize("add");
         objArray[this.index++] = element;
+        return true;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends T> c) {
+        if (c.size() > 0) {
+            System.arraycopy(c.toArray(), 0, objArray, 0, c.size());
+            this.index = c.size();
+        }
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
     }
 
     @Override
     public void add(int index, T element) {
-        objArray[index] = element;
-    }
-
-    @Override
-    public void addAll(ICustomList<T> list) {
-        if (list.size() > 0) {
-            System.arraycopy(list.getAll(), 0, objArray, 0, list.size());
-            this.index = list.size();
-        }
-    }
-
-    @Override
-    public void push(int index, T element) {
-        checkSize("add");
-        biasForward(index);
         objArray[index] = element;
     }
 
@@ -48,17 +77,46 @@ public class CustomArrayList<T> implements ICustomList<T> {
     }
 
     @Override
-    public T[] getAll() {
-        return objArray;
+    public T set(int index, T element) {
+        checkSize("add");
+        biasForward(index);
+        objArray[index] = element;
+        return null;
     }
 
     @Override
-    public void remove(int index) {
+    public T remove(int index) {
         if (index + 1 > this.index) {
             throw new ArrayIndexOutOfBoundsException("Element with this index didn't exists");
         }
         biasBack(index);
         checkSize("remove");
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<T> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<T> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<T> subList(int fromIndex, int toIndex) {
+        return null;
     }
 
     @Override
@@ -67,7 +125,32 @@ public class CustomArrayList<T> implements ICustomList<T> {
     }
 
     @Override
-    public void sort(Comparator<T> comparator) {
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T1> T1[] toArray(T1[] a) {
+        return null;
+    }
+
+    @Override
+    public void sort(Comparator<? super T> comparator) {
         T[] bufArray = (T[]) new Object[this.index];
         System.arraycopy(objArray.clone(), 0, bufArray, 0, this.index);
         Arrays.sort(bufArray, comparator);
