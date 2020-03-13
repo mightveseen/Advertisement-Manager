cb3ccb11a43048ed3c2daff539b92ac31ce533fc
petushok_valiantsin <blackvalik@gmail.com>
2020-03-13 11:09:18 +0300

    Maven

--- Hotel1/utility/src/main/java/com/senlainc/git_courses/java_training/petushok_valiantsin/utility/data/LoadData.java
+++ Hotel1/utility/src/main/java/com/senlainc/git_courses/java_training/petushok_valiantsin/utility/data/LoadData.java
@@ -1,7 +1,6 @@
 package com.senlainc.git_courses.java_training.petushok_valiantsin.utility.data;
 
 import com.senlainc.git_courses.java_training.petushok_valiantsin.dependency.injection.DependencyController;
-import com.senlainc.git_courses.java_training.petushok_valiantsin.utility.base_conection.ConnectionManager;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
@@ -13,9 +12,6 @@ public class LoadData {
     }
 
     public static void execute(Class<?> clazz) {
-        /* Create connection to DataBase */
-        DependencyController.getInstance().setDependency(ConnectionManager.class);
-        LOGGER.log(Level.DEBUG, "Successful initialize class: ConnectionManager");
         /* Create instance's of project classes */
         DependencyController.getInstance().setDependency(clazz);
         LOGGER.log(Level.DEBUG, "Successful initialize class: " + clazz.getSimpleName());
