906e5f78d4787ea03f050204d002cc19c44e8be4
Valiantsin Petushok <mightveseen@gmail.com>
2020-10-19 17:17:04 +0300

    Lombok modification

--- AdvertisementProjectG/controller/src/main/java/com/senlainc/javacourses/petushokvaliantsin/KafkaController.java
+++ AdvertisementProjectG/controller/src/main/java/com/senlainc/javacourses/petushokvaliantsin/KafkaController.java
@@ -1,9 +1,8 @@
 package com.senlainc.javacourses.petushokvaliantsin;
 
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 public class KafkaController {
 
     @KafkaListener(topics = "Hello", groupId = "app.id")
--- AdvertisementProjectG/controller/src/main/java/com/senlainc/javacourses/petushokvaliantsin/Start.java
+++ AdvertisementProjectG/controller/src/main/java/com/senlainc/javacourses/petushokvaliantsin/Start.java
@@ -1,19 +1,9 @@
 package com.senlainc.javacourses.petushokvaliantsin;
 
-import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
-
-@EnableKafka
+//@EnableKafka
 @SpringBootApplication
 public class Start {
 
@@ -21,19 +11,19 @@ public class Start {
         SpringApplication.run(Start.class, args);
     }
 
-    @Bean
-    public ConsumerFactory<String, String> consumerFactory() {
-        Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        return new DefaultKafkaConsumerFactory<>(configProps);
-    }
-
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory() {
-        final ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
-        listenerContainerFactory.setConsumerFactory(consumerFactory());
-        return listenerContainerFactory;
-    }
+//    @Bean
+//    public ConsumerFactory<String, String> consumerFactory() {
+//        Map<String, Object> configProps = new HashMap<>();
+//        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+//        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+//        return new DefaultKafkaConsumerFactory<>(configProps);
+//    }
+//
+//    @Bean
+//    public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory() {
+//        final ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
+//        listenerContainerFactory.setConsumerFactory(consumerFactory());
+//        return listenerContainerFactory;
+//    }
 }
