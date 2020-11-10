5bc18e3012f4bf4391f0aedb4d9440f72ba493e9
mightveseen <mightveseen@gmail.com>
2020-11-10 11:34:36 +0300

    Remove Kafka realisation

--- AdvertisementProjectG/controller/src/main/java/com/senlainc/javacourses/petushokvaliantsin/Start.java
+++ AdvertisementProjectG/controller/src/main/java/com/senlainc/javacourses/petushokvaliantsin/Start.java
@@ -3,27 +3,10 @@ package com.senlainc.javacourses.petushokvaliantsin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableKafka
 @SpringBootApplication
 public class Start {
 
     public static void main(String[] args) {
         SpringApplication.run(Start.class, args);
     }
-
-//    @Bean
-//    public ConsumerFactory<String, String> consumerFactory() {
-//        Map<String, Object> configProps = new HashMap<>();
-//        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-//        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-//        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-//        return new DefaultKafkaConsumerFactory<>(configProps);
-//    }
-//
-//    @Bean
-//    public ConcurrentKafkaListenerContainerFactory<String, String> concurrentKafkaListenerContainerFactory() {
-//        final ConcurrentKafkaListenerContainerFactory<String, String> listenerContainerFactory = new ConcurrentKafkaListenerContainerFactory<>();
-//        listenerContainerFactory.setConsumerFactory(consumerFactory());
-//        return listenerContainerFactory;
-//    }
 }
